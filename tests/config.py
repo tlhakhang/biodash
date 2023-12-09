81f7ce6184a304b151870ff699021906a4b9731d
tenzin <tenzin.c.lhakhang@icloud.com>
2023-12-09 00:50:01 -0500

    unit test add to flask model

--- tests/config.py
+++
@@ -1,5 +0,0 @@
-class TestConfig:
-    TESTING = True
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'  # Use an in-memory database for testing
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    WTF_CSRF_ENABLED = False
--- tests/unit/test_models.py
+++ tests/unit/test_models.py
@@ -1,5 +1,28 @@
+import pytest
+
 def test_always_passes():
     assert True
 
-def test_always_fails():
-    assert False
+def test_uppercase():
+    assert "loud noises".upper() == "LOUD NOISES"
+
+def test_reversed():
+    assert list(reversed([1, 2, 3, 4])) == [4, 3, 2, 1]
+
+def test_some_primes():
+    assert 37 in {
+        num
+        for num in range(2, 50)
+        if not any(num % div == 0 for div in range(2, num))
+    }
+
+@pytest.fixture
+def example_fixture():
+    return 1
+
+def test_with_fixture(example_fixture):
+    assert example_fixture == 1
+
+def test_index(app, client):
+    res = client.get('/')
+    assert res.status_code == 200
